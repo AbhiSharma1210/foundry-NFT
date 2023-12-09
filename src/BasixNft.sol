0817d0d4dfc482461327143dc8b631b8ecb04a35
Abhinav Sharma <abhi5237@gmail.com>
2023-12-09 14:54:49 +0530

    Basic NFT setup

---
+++ src/BasixNft.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@OpenZeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {}
+}
