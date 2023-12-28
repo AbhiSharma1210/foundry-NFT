f1e171cb0746b89357769c1015c9368102549cd5
Abhinav Sharma <abhi5237@gmail.com>
2023-12-28 21:09:15 +0530

    v1.4 Unit test update

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {BasicNft} from "../src/BasixNft.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
 contract DeployBasicNft is Script {
     function run() external returns (BasicNft) {
--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -2,15 +2,18 @@
 
 pragma solidity ^0.8.19;
 
-import {test} from "forge-std/Test.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
 import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
-import {BasicNft} from "../src/BasixNft.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
-    
-    funtion setUp() public() {
+    address public USER = makeAddr("user");
+    string public constant SHIBA =
+        "ipfs://bafybeig2alno34qtpwhvgsy3zp5khirprpcxncf4udrjdnl3lf4esbaxtm/";
+
+    function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
     }
@@ -18,6 +21,21 @@ contract BasicNftTest is Test {
     function testName_is_correct() public view {
         string memory expectedName = "Doggie";
         string memory actualName = basicNft.name();
-        
+
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCan_Mint_and_have_a_balance() public {
+        vm.prank(USER);
+        basicNft.mintNft(SHIBA);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(SHIBA)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
