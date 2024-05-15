170fc1f21d96419e97f13967bc2ef051bd5a871a
Abhinav Sharma <abhi5237@gmail.com>
2024-05-15 21:09:28 +0530

    comments

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@OpenZeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@OpenZeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
 
+    // We need a token ID to represent each unique NFT in a collection
+    // Since tokens in the same collection can have different worth
+    // here BasicNFT acts as a collection of tokens.
     constructor() ERC721("Doggie", "DOG") {
         s_tokenCounter = 0;
     }
